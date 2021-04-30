d942bc5ce151368da47221e5b881fadac7df4b91
InoDev-TaeGwan <development.s@iuproject.me>
2021-04-30 18:24:49 +0900

    Add Signup

--- src/components/Image.js
+++ src/components/Image.js
@@ -11,12 +11,14 @@ const StyledImage = styled.Image`
   background-color: ${({ theme }) => theme.imageBackground};
   width: 100px;
   height: 100px;
+  border-radius: ${({ rounded }) => (rounded ? 50 : 0)}px;
+  // 사용자의 사진을 원형으로 렌더링하기 위해 Image 컴포넌트에서 props를 통해 전달되는 값에 따라 이미지가 원형으로 렌더링되도록 함.
 `;
 
-const Image = ({ url, imageStyle }) => {
+const Image = ({ url, imageStyle, rounded }) => {
   return (
     <Container>
-      <StyledImage source={{ uri: url }} style={imageStyle} />
+      <StyledImage source={{ uri: url }} style={imageStyle} rounded={rounded} />
     </Container>
   );
 };
@@ -24,6 +26,7 @@ const Image = ({ url, imageStyle }) => {
 Image.propTypes = {
   uri: PropTypes.string,
   imageStyle: PropTypes.object,
+  rounded: PropTypes.bool,
 };
 
 export default Image;
